ab00b7e194185af97694f83d178fa0259a67fe12
Bilal Doğan <bilal.dogan@loodos.com>
2019-12-01 01:02:38 +0300

    Initial Commit

---
+++ DigitalMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitalMenu
+//
+//  Created by Bilal Doğan on 1.12.2019.
+//  Copyright © 2019 Bilal Doğan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
